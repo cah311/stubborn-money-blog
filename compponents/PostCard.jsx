2fac688a03197333e3181fef370e6329ff7deda8
Carson Halter <84420466+cah311@users.noreply.github.com>
2023-06-18 07:41:33 -0600

    add PostCard with image and title

--- compponents/PostCard.jsx
+++ compponents/PostCard.jsx
@@ -1,10 +1,33 @@
 import React from "react";
+import moment from "moment";
+import Link from "next/link";
 
 const PostCard = ({ post }) => {
+  console.log(post);
   return (
-    <div>
-      {post.title}
-      {post.excerpt}
+    <div className="bg-green-600 shadow-lg rounded-lg p-0 lg:p-8 pb-12 mb-8">
+      <div className="relative overflow-hidden shadow-md pb-80 mb-6">
+        <img
+          src={post.featuredImage.url}
+          alt={post.title}
+          className="object-top absolute h-80 w-full object-cover shadow-lg rounded-t-lg lg:rounded-lg"
+        />
+      </div>
+      <h1 className="transition duration-600 text-center mb-8 cursor-pointer hover:text-pink-600 text-3xl font-semibold">
+        <Link href={`/post/${post.slug}`}>{post.title}</Link>
+      </h1>
+      <div className="bloc lg:flex text-center items-center justify-center mb-8 w-full">
+        <div className="flex items-center justify-center mb-4 lg:mb-0 w-fulll lg:w-auto mr-8">
+          <img
+            alt={post.author.name}
+            height="60px"
+            width="60px"
+            className="align-middle rounded-full"
+            src={post.author.photo.url}
+          />
+          <p>{post.author.name}</p>
+        </div>
+      </div>
     </div>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,6 @@ import Head from "next/head";
 import { PostCard, Categories, PostWidget } from "../compponents";
 import { getPosts } from "../services";
 
-// const posts = [
-//   { title: "React Testing", excerpt: "Learn Rest Testing" },
-//   { title: "React with Tailwind", excerpt: "Learn Rest with Tailwind" },
-// ];
-
 export default function Home({ posts }) {
   return (
     <div className="container mx-auto px-10 mb-8 ">
@@ -15,10 +10,10 @@ export default function Home({ posts }) {
         <title>Stubborn Money</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="grid grid-cols-2 lg:grid-cols-12 gap-12">
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
           {posts.map((post) => (
-            <PostCard post={post} key={post.title} />
+            <PostCard post={post.node} key={post.title} />
           ))}
         </div>
         <div className="lg:col-span-4 col-span-1">
