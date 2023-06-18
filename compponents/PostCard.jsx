a31f557f9e157e4a41c69237d089d1024cfc15c5
Carson Halter <84420466+cah311@users.noreply.github.com>
2023-06-18 08:13:42 -0600

    add pageCard preview and 2nd pageCard

--- compponents/PostCard.jsx
+++ compponents/PostCard.jsx
@@ -25,8 +25,37 @@ const PostCard = ({ post }) => {
             className="align-middle rounded-full"
             src={post.author.photo.url}
           />
-          <p>{post.author.name}</p>
+          <p className="inline align-middle text-black ml-2 text-lg">
+            {post.author.name}
+          </p>
         </div>
+        <div className="font-medium text-black">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6 inline mr-2 text-black"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
+            />
+          </svg>
+          <span>{moment(post.createdAt).format("MMM DD, YYYY")}</span>
+        </div>
+      </div>
+      <p className="text-center text-lg text-black font-normal px-4 lg:px-20 mb-8">
+        {post.excerpt}
+      </p>
+      <div className="text-center">
+        <Link href={`/post/${post.slug}`}>
+          <span className="transition duration-500 transform fhover:-translate-y-1 inline-block bg-gray-300 text-lg font-medium rounded-full text-black px-6 py-3 cursor-pointer">
+            Continue Reading
+          </span>
+        </Link>
       </div>
     </div>
   );
