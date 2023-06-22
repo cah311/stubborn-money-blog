5a06e0f830901ef6c9d2a7224c7cac71b9b12d2a
Carson Halter <84420466+cah311@users.noreply.github.com>
2023-06-22 05:19:59 -0600

    add analytics

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Header } from "./";
+import { Analytics } from "@vercel/analytics/react";
 
 const Layout = ({ children }) => {
   return (
     <>
       <Header />
       {children}
+      <Analytics />
     </>
   );
 };
