478e3c182144db5afb58ae7c3a3380b7dde81ebc
Carson Halter <84420466+cah311@users.noreply.github.com>
2023-06-22 05:42:02 -0600

    move analytics to _app

--- pages/_app.js
+++ pages/_app.js
@@ -2,11 +2,13 @@ import React, { useEffect, useState } from "react";
 import { Layout } from "../components";
 import "tailwindcss/tailwind.css";
 import "../styles/globals.scss";
+import { Analytics } from "@vercel/analytics/react";
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Component {...pageProps} />
+      <Analytics />
     </Layout>
   );
 }
