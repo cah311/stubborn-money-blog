01a8f99449e8f9e4cb71fd3fca4c1188850c7212
Carson Halter <84420466+cah311@users.noreply.github.com>
2023-06-24 06:51:15 -0600

    fix headings error

--- components/PostDetail.jsx
+++ components/PostDetail.jsx
@@ -20,6 +20,22 @@ const PostDetail = ({ post }) => {
     }
 
     switch (type) {
+      case "heading-one":
+        return (
+          <h1 key={index} className="text-xl font-semibold mb-4">
+            {modifiedText.map((item, i) => (
+              <React.Fragment key={i}>{item}</React.Fragment>
+            ))}
+          </h1>
+        );
+      case "heading-two":
+        return (
+          <h2 key={index} className="text-xl font-semibold mb-4">
+            {modifiedText.map((item, i) => (
+              <React.Fragment key={i}>{item}</React.Fragment>
+            ))}
+          </h2>
+        );
       case "heading-three":
         return (
           <h3 key={index} className="text-xl font-semibold mb-4">
